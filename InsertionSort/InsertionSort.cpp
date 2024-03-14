2545261bbd91f7186a1e602ce21cd560bc76ec0f
Aswun <aswinlutfian32@gmail.com>
2024-03-14 15:08:30 +0700

    membuat display

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -51,6 +51,20 @@ void insertionSort()
     }
 }
 
+void display()
+{
+    cout << endl;
+    cout << "============================" << endl;
+    cout << "element array telah tersusun" << endl;
+    cout << "============================" << endl;
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j] << endl; // output setiap element pada garis baru
+    }
+    cout << "jumlah pass = " << n - 1 << endl; // menampilkan total
+    cout << endl;
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
